ad088190f4a20da1e1345eb96d8de07783583e7a
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2024-04-16 21:41:04 -0600

    Tarea 3: Netflix Eureka Client
    Se usa el cliente para que los microservicios puedan descubrir otros microservicios dentro del servidor

--- workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-celulares/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S21', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S22', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S23', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'Iphone 14', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'Iphone 15', NOW());
-INSERT INTO celulares (name, marca, created_at) VALUES ('Xiaomi', 'Mi', NOW());
