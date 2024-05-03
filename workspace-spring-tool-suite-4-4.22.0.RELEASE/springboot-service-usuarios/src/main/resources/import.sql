0bd1d6ce5e896c79e80ba909647860639a32e9d8
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2024-05-03 14:50:41 -0600

    Tarea 6 RepositoryRestResource HATEOAS
    Implementacion de RepositoryRestResource

---
+++ workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-usuarios/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('juanpa', '2024', 1, 'Juan', 'Ramos', 'pabloramos@uanl.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('volvoficial', '636469', 1, 'Volvo', 'S90', 'grupovolvo@gmail.com');
