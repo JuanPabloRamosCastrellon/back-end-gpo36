ccd164f6ce52954bbcac65f58bea063e118dcbf1
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2024-05-03 15:33:07 -0600

    Tarea 6 Probando CRUD Usuario
    Verificacion de operaciones CRUD para usuarios

--- workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-usuarios/src/main/resources/import.sql
+++ workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-usuarios/src/main/resources/import.sql
@@ -1,2 +1,10 @@
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('juanpa', '2024', 1, 'Juan', 'Ramos', 'pabloramos@uanl.com');
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('volvoficial', '636469', 1, 'Volvo', 'S90', 'grupovolvo@gmail.com');
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_to_roles (user_id, roles_id) VALUES (1, 1);
+INSERT INTO usuarios_to_roles (user_id, roles_id) VALUES (2, 2);
+INSERT INTO usuarios_to_roles (user_id, roles_id) VALUES (2, 1);
+
