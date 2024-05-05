2645abab4de20141bf18f0bc49d49a450d2f069a
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2024-05-04 19:46:46 -0600

    Tarea 6 Configurando Clientes
    Metodos para definir permisos en un endpoint y encriptar una contrasena y generar el token

--- workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-oauth/src/main/java/com/microservice/oauth/security/AuthorizationServerConfig.java
+++ workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-oauth/src/main/java/com/microservice/oauth/security/AuthorizationServerConfig.java
@@ -1,11 +1,15 @@
 package com.microservice.oauth.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -16,6 +20,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 
@@ -25,10 +32,28 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		
 	}
 	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()")
+		.checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("backEndApp")
+		.secret(passwordEncoder.encode("fcfm12345"))
+		.scopes("read", "write")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
+	}
+
+	@Bean
 	public JwtTokenStore jwtTokenStore() {
 		return new JwtTokenStore(jwtAccessTokenConverter());
 	}
 	
+	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		tokenConverter.setSigningKey("llave_pacifica_5x5");
