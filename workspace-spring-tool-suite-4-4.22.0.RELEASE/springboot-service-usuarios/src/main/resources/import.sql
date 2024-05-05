b0c3fe0dde784b70714efe952600a414602807ed
JuanPabloRamosCastrellon <jpramos.g44@gmail.com>
2024-05-04 21:13:46 -0600

    Tarea 6 Pruebas de Autenticacion
    Se verifica la autenticacion del cliente, encriptacion de contrasenas, generacion del token y decodificacion del mismo

--- workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-usuarios/src/main/resources/import.sql
+++ workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('juanpa', '2024', 1, 'Juan', 'Ramos', 'pabloramos@uanl.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('volvoficial', '636469', 1, 'Volvo', 'S90', 'grupovolvo@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('juanpa', '$2a$10$QrnqaYCIGd.w0qJdsDlse.v782fNPSjANFpoig9OGMDcWw0kOFfIm', 1, 'Juan', 'Ramos', 'pabloramos@uanl.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('volvoficial', '$2a$10$.KB8Vkk9vsZjPcbjLxSQJObpO5Mkh158QTMWPbJh8O6ujOeY.hjWq', 1, 'Volvo', 'S90', 'grupovolvo@gmail.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
